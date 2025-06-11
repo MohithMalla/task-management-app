14904d15b55dc00b37a95a265f9872719e5ba330
MohithMalla <mallamohith20@gmail.com>
2025-06-11 11:03:06 +0530

    Added complete frontend folder

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
-app.use("/api/auth", authRoutes);
+app.use("/api", authRoutes);
 app.use("/api", taskRoutes);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
