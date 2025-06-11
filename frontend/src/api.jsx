833b70086824931e78fc43fc0039299ecba03d7b
MohithMalla <167329211+MohithMalla@users.noreply.github.com>
2025-06-11 12:27:49 +0530

    Update api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-management-app-70a7.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
